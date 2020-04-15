100b3b2acf80e4beef9172cdea764c8c07f0e906
marckeelingiv <32782256+marckeelingiv@users.noreply.github.com>
2020-04-15 15:44:04 -0600

    Added static files and Post url

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import BlogListView
+from .views import BlogListView,BlogDetailView
 urlpatterns = [
+    path('post/<int:pk>/',BlogDetailView.as_view(),name='blogpost_detail'),
     path('',BlogListView.as_view(),name='blogposts'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 # Create your views here.
 class BlogListView(ListView):
     model = BlogPost
     template_name = 'blogpostlist.html'
     context_object_name = 'all_posts_list'
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blogpostdetial.html'
